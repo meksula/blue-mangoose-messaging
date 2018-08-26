c78aa604e0a61e54e7405e3f839ea9e05f54eb8e
meksula <meksula.karol.93@gmail.com>
2018-08-26 21:48:01 +0200

    Animated title added

--- blue-mangoose-client/src/main/java/com/bluemangoose/client/controller/HomeController.java
+++ blue-mangoose-client/src/main/java/com/bluemangoose/client/controller/HomeController.java
@@ -2,8 +2,10 @@ package com.bluemangoose.client.controller;
 
 import com.bluemangoose.client.controller.loader.FxmlLoader;
 import com.bluemangoose.client.controller.loader.FxmlLoaderTemplate;
+import com.bluemangoose.client.model.gui.TitleAnimation;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -18,15 +20,21 @@ import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
     private FxmlLoader fxmlLoader;
+    private TitleAnimation titleAnimation;
 
     @FXML
     private ImageView registerButton, loginButton;
+
+    @FXML
+    private Label b, l, u, e, m, a, n, g, o1, o2, s, e2;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.fxmlLoader = new FxmlLoaderTemplate();
         setLoginAction();
         setRegisterAction();
+        titleAnimation = new TitleAnimation(new Label[] {b, l, u, e, m, a, n, g, o1, o2, s, e2});
+        titleAnimation.animation();
     }
 
     private void setLoginAction() {
@@ -34,6 +42,7 @@ public class HomeController implements Initializable {
         loginButton.setOnMouseExited(event -> loginButton.setImage(new Image("/img/login.png")));
 
         loginButton.setOnMouseClicked(event -> {
+            titleAnimation.stop();
             final String path = "/templates/login.fxml";
             fxmlLoader.loadSameStage(path, event);
         });
@@ -45,9 +54,11 @@ public class HomeController implements Initializable {
         registerButton.setOnMouseExited(event -> registerButton.setImage(new Image("/img/register.png")));
 
         registerButton.setOnMouseClicked(event -> {
+            titleAnimation.stop();
             final String path = "/templates/registration.fxml";
             fxmlLoader.loadSameStage(path, event);
         });
+
     }
 
 }
---
+++ blue-mangoose-client/src/main/java/com/bluemangoose/client/model/gui/TitleAnimation.java
@@ -0,0 +1,118 @@
+package com.bluemangoose.client.model.gui;
+
+import javafx.scene.control.Label;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author
+ * Karol Meksuła
+ * 26-08-2018
+ * */
+
+public class TitleAnimation {
+    private AtomicBoolean run = new AtomicBoolean(true);
+    private AtomicInteger counter = new AtomicInteger(0);
+    private Label[] letters;
+
+    public TitleAnimation(Label[] letters) {
+        this.letters = letters;
+    }
+
+    public void animation() {
+        Runnable animation = () -> {
+            initState();
+
+            while (run.get()) {
+                letterAction();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                counter.incrementAndGet();
+            }
+
+        };
+
+        Thread animationThread = new Thread(animation);
+        animationThread.setDaemon(true);
+        animationThread.start();
+    }
+
+    private void letterAction() {
+        int index = counter.intValue();
+        int initX = index * 50;
+
+        Label current = null;
+        if (index < letters.length) {
+            current = letters[index];
+        }
+
+        if (counter.get() == 12) {
+            shineLetters();
+        }
+
+        while (current != null && current.getLayoutY() < 120) {
+            if (current.getStyleClass().size() == 0) {
+                current.getStyleClass().add("anim_label");
+            } else {
+                current.getStyleClass().clear();
+            }
+
+            double y = current.getLayoutY();
+            current.setLayoutX(initX);
+            current.setLayoutY(y + 10);
+
+            try {
+                Thread.sleep(80);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void shineLetters() {
+        int counter = 0;
+        final String STANDARD = "anim_label";
+        final String SHINE = "anim_label_shine";
+
+        while (counter < 10) {
+            for (Label letter : letters) {
+                try {
+                    if (letter.getStyleClass().size() == 0) {
+                        letter.getStyleClass().clear();
+                        letter.getStyleClass().add(STANDARD);
+                    } else {
+                        letter.getStyleClass().clear();
+                        if (counter % 2 == 0) {
+                            letter.getStyleClass().add(SHINE);
+                        } else {
+                            letter.getStyleClass().add(STANDARD);
+                        }
+                    }
+
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            counter ++;
+        }
+    }
+
+    public void stop() {
+        run.set(false);
+    }
+
+    private void initState() {
+        for (Label letter : letters) {
+            letter.setVisible(true);
+            letter.setLayoutX(-50);
+            letter.setLayoutY(-50);
+        }
+    }
+
+}
